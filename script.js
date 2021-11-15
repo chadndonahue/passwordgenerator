38c86b0554ce33058ef9eac5e7f939f6018d8135
Chad <chadndonahue@gmail.com>
2021-11-14 20:34:44 -0500

    added empty strings to fill with the password, also added the password length variable

--- script.js
+++ script.js
@@ -5,15 +5,59 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  // this returns undefined if nothing is entered
+  if(!password) return
   passwordText.value = password;
 
 }
-
+// These will be the variables that I can pick from
 var lowerCase = "abcdefghijklmnopqrstuvwxyz"
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var numbers = "0123456789" 
 var specialChar = "~@#$%^&*()_+|"
 
+// Setting two empty strings that will be filled 
+function generatePassword() {
+  var passwordCharacters = ""
+  var password = ""
+
+  var passwordLength = prompt("Please select a password length between 8 and 128")
+  if(!passwordLength) 
+  return
+
+  // isNan checks if a number is entered or not and will return false if it is not a number
+
+  if(isNaN)(passwordLength) {
+    alert("only enter a number dingus")
+    return
+  }
+  // This tells it what to do when someone enters a numbers below 8
+  if(passwordLength < 8) {
+    alert("dude it says 8 or above")
+    return
+  }
+  // This tells it what to do when someone enters a number above 128
+  if(passwordLength > 128) {
+    alert("man it says right there 128 or less")
+    return
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+// Goes on bottom of the page?
